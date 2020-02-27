497b620b5ce1c4f52311f476724e9fe159062d55
Jamrod <myfavoritenoisemaker@gmail.com>
2020-02-27 16:07:22 -0700

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
 
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Components/Home'
 import Music from './Components/Music'
-// import Scripts from './Components/Scripts/Scripts'
-import WhoGoesFirst from './Components/WhoGoesFirst/WhoGoesFirst'
-import ScaleFinder from './Components/ScaleFinder/ScaleFinder'
 import Projects from './Components/Projects'
 
 import './App.css';
 
 function App() {
+  const history = createBrowserHistory();
+
+  const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+  if (path) {
+    history.replace(path);
+  }
   return (
     <div className="App flex-container-column" styles={{ backgroundImage: `url(milky-way-starry-sky-night-sky-star-956981.jpeg)` }}>
       <div className="container flex-container-column">
@@ -22,8 +26,6 @@ function App() {
           <Route path="/" component={Projects} exact></Route>
           <Route path="/About/" component={Home} ></Route>
           <Route path="/Music" component={Music}></Route>
-          <Route path="/WhoGoesFirst/" component={WhoGoesFirst}></Route>
-          <Route path="/ScaleFinder/" component={ScaleFinder}></Route>
           <Route path="/Projects/" component={Projects}></Route>
         </Switch>
 
