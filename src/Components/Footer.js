a2ebecbc78b84ff64dbe15d7fabe99639116dd24
Jamrod <myfavoritenoisemaker@gmail.com>
2020-09-01 17:08:49 -0600

    going to merge edits

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
 
     return (
         <div className="flex-container-row footer">
-            <div className="foot flex-container-row">
+            <div className="foots flex-container-row">
                 <span>&hearts; &copy; 2020</span>
-                <img alt="JR" class="icon" src="/assets/img/JRsymbol.png" />
+                <img alt="JR" class="icons" src="/JRsymbol.png" />
                 <span>JCR &hearts;</span>
             </div>
-            <div className="flex-container-row">
+            <div className="flex-container-row foots">
                 <a href="mailto:jamcrodgers@gmail.com?subject=Web Contact"><img src="./icons/mail.svg" alt="email" className="icons" /></a>
                 <a href="http://github.com/jamrod" ><img src="./Octocat.png" alt="github" className="icons" /></a>
                 <a href="http://www.linkedin.com/in/jamescrodgers"><img src="./LI-In-Bug.png" alt="linked in" className="icons" /></a>
