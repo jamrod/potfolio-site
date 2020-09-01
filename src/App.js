a7fd30e5b627737e9ee8de51f2ab51cb28757be8
Jamrod <myfavoritenoisemaker@gmail.com>
2020-08-31 20:46:34 -0600

    new footer and cloud

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App flex-container-column">
+      <div className="App">
         <div className="container flex-container-column">
           <Header></Header>
           <Nav active={this.state.active}></Nav>
